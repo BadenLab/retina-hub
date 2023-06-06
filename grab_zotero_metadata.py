16eb8217d6807c5e7a4c2f67bdd9341d5138e592
amchagas <andremaia.chagas@gmail.com>
2023-06-06 16:06:34 +0100

    update code and push v0.1 of python code

--- grab_zotero_metadata.py
+++ grab_zotero_metadata.py
@@ -4,15 +4,15 @@ Small script to load necessary libraries and match google scholar entries to Web
 import requests
 import pandas as pd
 from pyzotero import zotero
-import json
-import time
+#import json
+#import time
 
 
 
 
-sheet_id = "14EgPuHwmltEjWeFZfwygVKYwSlvYkG5yCv6IXquz2Os"
-sheet_name = "Form Responses 1"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+#sheet_id = "14EgPuHwmltEjWeFZfwygVKYwSlvYkG5yCv6IXquz2Os"
+#sheet_name = "Form Responses 1"
+#url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
 
 sheet_url = "https://docs.google.com/spreadsheets/d/14EgPuHwmltEjWeFZfwygVKYwSlvYkG5yCv6IXquz2Os/edit#gid=1702410538"
@@ -50,20 +50,21 @@ collections = {
   "frog":"FKN3BCFK",
   "human":"6B6UBDAK",
   "mouse":"K93FPB4B",
-  "non human primate":"U39PBZ7C",
+  "non-human primate":"U39PBZ7C",
   "other":"WDSXVYN6",
   "rat":"T4AYDE66",
   "zebrafish":"33DJT8BK",
-},
+  "shark":"Z4IN47WJ"
+  },
   "pub":{
   "book":"IL925QCT",
   "other":"WACYJQPH",
   "peer reviewed":"N3H5YAW3",
   "preprint":"UAYZHHPY",
   "review":"UF9V5CB6",
-},
+  },
   "area":{
-  "Computational":"QVN2AP8C",
+  "computational":"QVN2AP8C",
   "function":"ICYG3YV6",
   "injury/disease":"NI8DPUXS",
   "inner retina":"J5IMW5BP",
@@ -72,16 +73,16 @@ collections = {
   "other":"ZWVM2ZIS",
   "outer retina":"D8SDAZI7",
   "structure":"R5ZD6ZLM",
-},
+  },
   "cell":{
-  "Amacrine":"2ECXQ9WE",
+  "amacrine":"2ECXQ9WE",
   "bipolar":"78A6KXVR",
   "cones":"66QHAKSP",
   "horizontal":"M6CLZYGV",
   "other":"UZD6C2J9",
   "retinal ganglion":"8QY5WMJH",
   "rods":"4WMPKKHJ"
-  }
+   }
   }
 
 #dois = articles.iloc[:,3]
@@ -106,13 +107,11 @@ headers = {"content-type": "text/plain", "Accept-Charset": "UTF-8"}
 index=0
 #allMeta = list()
 
-#find all comma occurences in a string
-def findOccurrences(s, ch):
-    return [i for i, letter in enumerate(s) if letter == ch]
 
 
 for idx in items_to_add.index:
-    try:
+    if str(items_to_add.loc[idx][g_doi_column])!="nan":
+        print(idx)
         r = requests.post(url=url, data=items_to_add.loc[idx][g_doi_column], headers=headers)
         temp = r.json()
          
@@ -128,7 +127,7 @@ for idx in items_to_add.index:
         species = items_to_add.loc[idx]["Species (select all that apply)"]
         species = species.split(",")
         for animal in species:
-            animal = animal.lower()
+            animal = animal.lower().strip()
                 
         
             if animal=="frog":
@@ -137,79 +136,108 @@ for idx in items_to_add.index:
                 zot.addto_collection(collections["animals"]["human"],zot.item(entry_key))    
             if animal=="mouse":
                 zot.addto_collection(collections["animals"]["mouse"],zot.item(entry_key))    
-            if animal=="non human primate":
-                zot.addto_collection(collections["animals"]["non human primate"],zot.item(entry_key))    
-            if animal=="other":
-                zot.addto_collection(collections["animals"]["other"],zot.item(entry_key))    
+            if animal=="non-human primate":
+                zot.addto_collection(collections["animals"]["non-human primate"],zot.item(entry_key))    
+
             if animal=="rat":
                 zot.addto_collection(collections["animals"]["rat"],zot.item(entry_key))    
             if animal=="zebrafish":
-                zot.addto_collection(collections["animals"]["zebrafish"],zot.item(entry_key))    
-        
+                zot.addto_collection(collections["animals"]["zebrafish"],zot.item(entry_key))
+            if animal=="shark":
+                zot.addto_collection(collections["animals"]["shark"],zot.item(entry_key))
+            if animal!="frog" and \
+               animal!="non-human primate"and \
+               animal!="rat"and \
+               animal!="zebrafish" and\
+               animal!="human" and\
+                 animal!="chicken" and\
+                animal!="shark" and\
+               animal!="mouse":
+                print(animal)
+                zot.addto_collection(collections["animals"]["other"],zot.item(entry_key))                
+
         #publication type
         pub_type = items_to_add.loc[idx]["Type"]
-        for entry in pub_type :
-            for sub_entry in entry.split(","):
-                publication = sub_entry.strip()
-        
-                if publication.lower()=="peer reviewed":
-                    zot.addto_collection(collections["pub"]["peer reviewed"],zot.item(entry_key))    
-                if publication.lower()=="book":
-                    zot.addto_collection(collections["pub"]["book"],zot.item(entry_key))          
-                if publication.lower()=="other":
-                    zot.addto_collection(collections["pub"]["other"],zot.item(entry_key))    
-                if publication.lower()=="preprint":
-                    zot.addto_collection(collections["pub"]["preprint"],zot.item(entry_key))          
-                if publication.lower()=="review":
-                    zot.addto_collection(collections["pub"]["review"],zot.item(entry_key))          
-        
+        pub_type = pub_type.split(",")
+        for publication in pub_type:
+            publication = publication.lower().strip()
+                
+            if publication=="peer reviewed":
+                zot.addto_collection(collections["pub"]["peer reviewed"],zot.item(entry_key))    
+            if publication=="book":
+                zot.addto_collection(collections["pub"]["book"],zot.item(entry_key))          
+            if publication=="preprint":
+                zot.addto_collection(collections["pub"]["preprint"],zot.item(entry_key))          
+            if publication=="review":
+                zot.addto_collection(collections["pub"]["review"],zot.item(entry_key))    
+            if publication!="book" and\
+               publication!="peer reviewed" and\
+               publication!="preprint" and\
+               publication!="review":
+                print(publication)
+                zot.addto_collection(collections["pub"]["other"],zot.item(entry_key))    
      #main areas
         main_area = items_to_add.loc[idx]["Main areas (please select all that apply)"]
-        for entry in main_area :
-            for sub_entry in entry.split(","):
-                area = sub_entry.strip()
-                if area.lower()=="computational":
-                    zot.addto_collection(collections["area"]["computational"],zot.item(entry_key))       
-                if area.lower()=="function":
-                    zot.addto_collection(collections["area"]["function"],zot.item(entry_key))
-                if area.lower()=="injury/disease":
-                    zot.addto_collection(collections["area"]["injury/disease"],zot.item(entry_key))       
-                if area.lower()=="inner retina":
-                    zot.addto_collection(collections["area"]["inner retina"],zot.item(entry_key))       
-                if area.lower()=="molecular":
-                    zot.addto_collection(collections["area"]["molecular"],zot.item(entry_key))       
-                if area.lower()=="organoid":
-                    zot.addto_collection(collections["area"]["organoid"],zot.item(entry_key))       
-                if area.lower()=="other":
-                    zot.addto_collection(collections["area"]["other"],zot.item(entry_key))       
-                if area.lower()=="outer retina":
-                    zot.addto_collection(collections["area"]["outer retina"],zot.item(entry_key))       
-                if area.lower()=="structure":
-                    zot.addto_collection(collections["area"]["structure"],zot.item(entry_key))       
-
+        main_area = main_area.split(",")
+        for area in main_area :
+            area = area.lower().strip()
+            if area=="computational":
+                zot.addto_collection(collections["area"]["computational"],zot.item(entry_key))       
+            if area=="function":
+                zot.addto_collection(collections["area"]["function"],zot.item(entry_key))
+            if area=="injury/disease":
+                zot.addto_collection(collections["area"]["injury/disease"],zot.item(entry_key))       
+            if area=="inner retina":
+                zot.addto_collection(collections["area"]["inner retina"],zot.item(entry_key))       
+            if area=="molecular":
+                zot.addto_collection(collections["area"]["molecular"],zot.item(entry_key))       
+            if area=="organoid":
+                zot.addto_collection(collections["area"]["organoid"],zot.item(entry_key))       
+            #elif area=="other":
+            #    zot.addto_collection(collections["area"]["other"],zot.item(entry_key))       
+            if area=="outer retina":
+                zot.addto_collection(collections["area"]["outer retina"],zot.item(entry_key))       
+            if area=="structure":
+                zot.addto_collection(collections["area"]["structure"],zot.item(entry_key))       
+            if area != "structure" and \
+                area!= "outer retina" and \
+                area!="organoid" and\
+                area!="molecular" and\
+                area!="inner retina" and\
+                area!="injury/disease" and\
+                area!="function" and\
+                area!="computational":
+                print(area)
+                zot.addto_collection(collections["area"]["other"],zot.item(entry_key))       
 
         #celltype
         cell_type = items_to_add.loc[idx]["cell types (select all that apply)"]
-        for entry in cell_type :
-            for sub_entry in entry.split(","):
-                cell = sub_entry.strip()
-                if cell.lower()=="amacrine":
-                    zot.addto_collection(collections["cell"]["amacrine"],zot.item(entry_key))       
-                if cell.lower()=="bipolar":
-                    zot.addto_collection(collections["cell"]["bipolar"],zot.item(entry_key))       
-                if cell.lower()=="cones":
-                    zot.addto_collection(collections["cell"]["cones"],zot.item(entry_key))       
-                if cell.lower()=="horizontal":
-                    zot.addto_collection(collections["cell"]["horizontal"],zot.item(entry_key))       
-                if cell.lower()=="other":
-                    zot.addto_collection(collections["cell"]["other"],zot.item(entry_key))       
-                if cell.lower()=="retinal ganglion":
-                    zot.addto_collection(collections["cell"]["retinal ganglion"],zot.item(entry_key))       
-                if cell.lower()=="rods":
-                    zot.addto_collection(collections["cell"]["rods"],zot.item(entry_key))       
-    
-    except:
-        print("error")
+        cell_type = cell_type.split(",")
+        for cell in cell_type :
+            cell = cell.lower().strip()
+            if cell=="amacrine cells":
+                zot.addto_collection(collections["cell"]["amacrine"],zot.item(entry_key))       
+            if cell=="bipolar cells":
+                zot.addto_collection(collections["cell"]["bipolar"],zot.item(entry_key))       
+            if cell=="cones":
+                zot.addto_collection(collections["cell"]["cones"],zot.item(entry_key))       
+            if cell=="horizontal cells":
+                zot.addto_collection(collections["cell"]["horizontal"],zot.item(entry_key))       
+            #if cell=="other":
+            #    zot.addto_collection(collections["cell"]["other"],zot.item(entry_key))       
+            if cell=="retinal ganglion cells":
+                zot.addto_collection(collections["cell"]["retinal ganglion"],zot.item(entry_key))       
+            if cell=="rods":
+                zot.addto_collection(collections["cell"]["rods"],zot.item(entry_key))       
+            if cell!="rods" and cell!="cones" and \
+                cell!="horizontal cells" and\
+                cell!="bipolar cells" and\
+                cell!="amacrine cells" and\
+                cell !="retinal ganglion cells":
+                print(cell)
+                zot.addto_collection(collections["cell"]["other"],zot.item(entry_key))       
+                
+
 
 #with open(dataPath + "zotMeta.json", "w") as fid:
 #    json.dump(allMeta, fid)
